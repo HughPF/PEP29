4f12ca54999fa978f0ce4f9befc229896acbf113
HughPF <126900977+HughPF@users.noreply.github.com>
2025-09-30 08:47:43 +0200

    Add files via upload

---
+++ U1/programas_01/programa01.py
@@ -0,0 +1,4 @@
+# programa01
+# Escribe un programa que muestre por pantalla tú nombre y apellidos.
+
+print("Hugo Pérez Fermoso")
---
+++ U1/programas_01/programa02.py
@@ -0,0 +1,23 @@
+# programa02
+# Escribe un programa que escriba por pantalla: el booleano True, dos literales enteros, dos literales flotantes y una cadena de caracteres, y para cada uno de ellos su tipo de datos.
+
+
+# Booleano
+b = True
+print(b, type(b))
+
+# Literales enteros
+i1 = 42
+i2 = -7
+print(i1, type(i1))
+print(i2, type(i2))
+
+# Literales flotantes
+f1 = 3.14
+f2 = -0.001
+print(f1, type(f1))
+print(f2, type(f2))
+
+# Cadena de caracteres
+s = "Hola, mundo"
+print(s, type(s))
